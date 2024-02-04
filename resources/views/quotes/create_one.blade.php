df6083b5995d1a7804b0dc004339456bef438c90
Hristo Hristov <koredalin@gmail.com>
2024-02-04 22:48:10 +0200

    Add Quote styling

--- resources/views/quotes/create_one.blade.php
+++ resources/views/quotes/create_one.blade.php
@@ -13,34 +13,34 @@
                 </div>
             @endif
         </div>
-        <form method="POST" action="{{ route('quotes.add_one') }}">
+        <form method="POST" action="{{ route('quotes.add_one') }}" class="max-w-sm mx-auto">
             @csrf
-            <div class="form-group">
-                <label for="question">Question:</label>
-                <input type="text" class="form-control" id="question" name="question" required>
+            <div class="mb-5">
+                <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question:</label>
+                <input type="text" class="form-control" id="question" name="question" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
             </div>
-            <div class="form-group">
-                <label for="mode">Mode:</label>
-                <select class="form-control" id="mode" name="mode" required>
+            <div class="mb-5">
+                <label for="mode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mode:</label>
+                <select class="form-control" id="mode" name="mode" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
                     <option value="binary">Binary</option>
                     <option value="multiple_choice">Multiple Choice</option>
                 </select>
             </div>
-            <div class="form-group">
-                <label for="answer_a">Answer A:</label>
-                <input type="text" class="form-control" id="answer_a" name="answer_a">
+            <div class="mb-5">
+                <label for="answer_a" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Answer A:</label>
+                <input type="text" class="form-control" id="answer_a" name="answer_a" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
-            <div class="form-group">
-                <label for="answer_b">Answer B:</label>
-                <input type="text" class="form-control" id="answer_b" name="answer_b">
+            <div class="mb-5">
+                <label for="answer_b" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Answer B:</label>
+                <input type="text" class="form-control" id="answer_b" name="answer_b" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
-            <div class="form-group">
-                <label for="answer_c">Answer C:</label>
-                <input type="text" class="form-control" id="answer_c" name="answer_c">
+            <div class="mb-5">
+                <label for="answer_c" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Answer C:</label>
+                <input type="text" class="form-control" id="answer_c" name="answer_c" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
-            <div class="form-group">
-                <label for="right_answer">Your Answer:</label>
-                <select class="form-control" id="right_answer" name="right_answer" required>
+            <div class="mb-5">
+                <label for="right_answer" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Answer:</label>
+                <select class="form-control" id="right_answer" name="right_answer" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
                     <option value="">Plese, select</option>
                     <option value="0">0</option>
                     <option value="1">1</option>
@@ -49,7 +49,7 @@
                     <option value="C">C</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary">Create Quote</button>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Quote</button>
         </form>
 
         <div id="internal_links">
